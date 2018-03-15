626f26199296ee579d69352ecd78d583e37122fc
Elk Cloner <Elk Cloner>
2018-03-15 13:54:47 +0330

    wifi name

---
+++ main.py
@@ -0,0 +1,27 @@
+__author__ = "Amir Mohammad"
+
+import subprocess
+
+command_one = "netsh wlan show profiles"
+var = "All User Profile"
+
+
+a = subprocess.check_output(command_one.split(" ")).decode('utf-8').split('\n')
+a = [i for i in a if "All User Profile" in i]
+
+
+c = 0
+for i in a:
+                
+        c+=1        
+        #print i
+        print a
+
+
+
+
+
+#print i
+#resault = subprocess.check_output(['netsh','wlan','show','profiles',i,'key=clear']).decode('utf-8').split('\n')
+#resault = [b for b in resault if "Key Content" in b]
+
