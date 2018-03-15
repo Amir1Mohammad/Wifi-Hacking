ed08f24dcd5a7a325b3e31a504fab1fd80d50b2d
Elk Cloner <Elk Cloner>
2018-03-15 20:32:22 +0330

    show the wifi password

--- main.py
+++ main.py
@@ -4,22 +4,20 @@ import subprocess
 
 command_one = "netsh wlan show profiles"
 var = "All User Profile"
-
+wifi_list=[]
 
 a = subprocess.check_output(command_one.split(" ")).decode('utf-8').split('\n')
 a = [i for i in a if "All User Profile" in i]
 c=0
 for i in a:
-        c+=1
         
-        t = i.split(':')[1].replace('\r', '')
-        t = t.strip()
-        print t
-        
-
-
-
-#print i
-#resault = subprocess.check_output(['netsh','wlan','show','profiles',i,'key=clear']).decode('utf-8').split('\n')
-#resault = [b for b in resault if "Key Content" in b]
-
+        try:
+                t = i.split(':')[1].replace('\r', '')
+                t = t.strip()
+                resault = subprocess.check_output(['netsh','wlan','show','profile',t,'key=clear']).decode('utf-8').split('\n')
+                resault = [b for b in resault if "Key Content" in b]
+                
+                print t," : ",resault
+        #print resault
+        except:
+                print ""
